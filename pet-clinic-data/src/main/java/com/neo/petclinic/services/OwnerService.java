be10b88d0d96e6293b766daf4adbe9bc4e515e5a
Neo <omkarkandale@gmail.com>
2020-07-06 23:55:48 +0530

    Implement Interfaces for services. Closes #3

---
+++ pet-clinic-data/src/main/java/com/neo/petclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.neo.petclinic.services;
+
+import com.neo.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
