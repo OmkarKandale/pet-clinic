225c88697c3e38c7bf3e41f59add110e8bb49403
Neo <omkarkandale@gmail.com>
2020-07-07 15:13:28 +0530

    Refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/com/neo/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/neo/petclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package com.neo.petclinic.services;
 
 import com.neo.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
