a40d44a65c7ed674b1d76c3a93df590906edce2d
Neo <omkarkandale@gmail.com>
2020-07-08 21:49:59 +0530

    Adding spring config. closes #14

--- pet-clinic-data/src/main/java/com/neo/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/neo/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.neo.petclinic.services.map;
 
 import com.neo.petclinic.model.Pet;
 import com.neo.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-web/src/main/java/com/neo/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/neo/petclinic/bootstrap/DataLoader.java
@@ -4,19 +4,20 @@ import com.neo.petclinic.model.Owner;
 import com.neo.petclinic.model.Vet;
 import com.neo.petclinic.services.OwnerService;
 import com.neo.petclinic.services.VetService;
-import com.neo.petclinic.services.map.OwnerServiceMap;
-import com.neo.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
