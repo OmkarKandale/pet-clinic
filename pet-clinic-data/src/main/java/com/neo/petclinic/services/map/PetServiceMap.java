d9c8cbe86c630b879b2ce05bf86f871ed6f953b3
Neo <omkarkandale@gmail.com>
2020-07-08 18:03:28 +0530

    Adding bootstrap to data. closes #14

--- pet-clinic-data/src/main/java/com/neo/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/neo/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.neo.petclinic.services.map;
 
 import com.neo.petclinic.model.Pet;
-import com.neo.petclinic.services.CrudService;
+import com.neo.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/neo/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/neo/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.neo.petclinic.services.map;
 
 import com.neo.petclinic.model.Vet;
-import com.neo.petclinic.services.CrudService;
+import com.neo.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
