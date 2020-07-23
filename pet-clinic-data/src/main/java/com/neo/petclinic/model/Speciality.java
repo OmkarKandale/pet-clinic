df8ff31a01ad0a61115fd191181e1b6691032217
Neo <omkarkandale@gmail.com>
2020-07-23 23:18:31 +0530

    Added project lombok to project, closes #52

--- pet-clinic-data/src/main/java/com/neo/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/neo/petclinic/model/Speciality.java
@@ -1,9 +1,16 @@
 package com.neo.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
